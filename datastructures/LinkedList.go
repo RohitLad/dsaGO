fd7eef2950b455b74d1fee639e508e2324bf4303
Rohit <rohit.lad.sae@gmail.com>
2019-10-26 13:06:13 +0200

    A rather good implementation of LinkedList.

--- datastructures/LinkedList.go
+++ datastructures/LinkedList.go
@@ -3,37 +3,86 @@ package datastructures
 import "fmt"
 
 type LinkedList struct {
-	Head *Node
+	head   *node
+	tail   *node
+	length int
 }
 
-type Node struct {
+type node struct {
 	Data interface{}
-	Next *Node
+	next *node
 }
 
-func (list *LinkedList) Insert(elem interface{}) {
-	node := Node{Data: elem}
-	if list.Head == nil {
-		list.Head = &node
+func (list *LinkedList) Append(elem interface{}) {
+	newNode := node{Data: elem}
+	if list.length == 0 {
+		list.head = &newNode
+		list.tail = list.head
 	} else {
-		currNode := list.Head
-		for currNode.Next != nil {
-			currNode = currNode.Next
-		}
-		currNode.Next = &node
+		lastNode := list.tail
+		lastNode.next = &newNode
+		list.tail = lastNode.next
 	}
+	list.length++
+}
+
+func (list *LinkedList) Pretend(elem interface{}) {
+	newNode := node{Data: elem, next: list.head}
+	list.head = &newNode
+	list.length++
+}
+
+func (list *LinkedList) Insert(index int, elem interface{}) {
+	if index == 0 {
+		list.Pretend(elem)
+	} else if index == list.length {
+		list.Append(elem)
+	} else {
+		newNode := node{Data: elem}
+		currNode := list.head
+		for i := 1; i <= index-1; i++ {
+			currNode = currNode.next
+		}
+		newNode.next = currNode.next
+		currNode.next = &newNode
+		list.length++
+	}
+
+}
+
+func (list *LinkedList) Remove(index int) {
+	if index == 0 {
+		list.head = list.head.next
+	} else {
+		currNode := list.head
+		for i := 1; i <= index-1; i++ {
+			currNode = currNode.next
+		}
+		removeNode := currNode.next
+		next2removeNode := removeNode.next
+		currNode.next = next2removeNode
+		if index == list.length-1 {
+			list.tail = currNode
+		}
+	}
+	list.length--
+}
+
+func (list LinkedList) Size() int {
+	return list.length
 }
 
 func (list LinkedList) Display() {
 
-	node := list.Head
+	node := list.head
 	if node == nil {
-		fmt.Println("Empty Linked List")
+		fmt.Println("[]")
 	} else {
 		for node != nil {
 			fmt.Printf("%v ->", node.Data)
-			node = node.Next
+			node = node.next
 		}
-		fmt.Println("")
+
 	}
+	fmt.Println("   Size: ", list.Size())
 }
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	ds "DSA/datastructures"
+	ds "dsaGO/datastructures"
 )
 
 func main() {
@@ -10,8 +10,15 @@ func main() {
 
 func testLinkedList() {
 	list := ds.LinkedList{}
-	list.Insert(55.0)
-	list.Insert(88)
-	list.Insert(68.125)
+	list.Append(55.0)
+	list.Append(88)
+	list.Append(68.125)
+	list.Pretend(458)
+	list.Display()
+	list.Insert(2, 33.5)
+	list.Display()
+	list.Remove(4)
+	list.Append(78)
+	list.Remove(1)
 	list.Display()
 }
