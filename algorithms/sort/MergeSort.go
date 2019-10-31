3270f507c0486e26ec05e7a04eeba377b898fad7
Rohit <rohit.lad@airbus.com>
2019-10-31 16:41:36 +0100

    Merge sort implementation, could be improved

---
+++ algorithms/sort/MergeSort.go
@@ -0,0 +1,39 @@
+package sort
+
+func mDivide(array []int, lIndex int, rIndex int) []int {
+	mid := (lIndex + rIndex) / 2
+	diff := rIndex - lIndex
+	if diff/2 == 0 {
+		return []int{array[lIndex]}
+	}
+	lArr := mDivide(array, lIndex, mid)
+	rArr := mDivide(array, mid, rIndex)
+	tmpArr := []int{}
+	lCount := 0
+	rCount := 0
+	for i := 0; i < len(lArr)+len(rArr); i++ {
+		if lCount == len(lArr) {
+			tmpArr = append(tmpArr, rArr[rCount:]...)
+			break
+		} else if rCount == len(rArr) {
+			tmpArr = append(tmpArr, lArr[lCount:]...)
+			break
+		} else {
+			if lArr[lCount] > rArr[rCount] {
+				tmpArr = append(tmpArr, rArr[rCount])
+				rCount++
+			} else {
+				tmpArr = append(tmpArr, lArr[lCount])
+				lCount++
+			}
+		}
+	}
+	return tmpArr
+}
+
+func MergeSort(list []int) {
+	sorted := mDivide(list, 0, len(list))
+	for i, _ := range list {
+		list[i] = sorted[i]
+	}
+}
