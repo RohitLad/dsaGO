23c16b83c65761578f51fb3c3b14ad7f698d1175
Rohit <rohit.lad.sae@gmail.com>
2019-11-02 12:13:59 +0100

    minor changes

--- algorithms/sort/QuickSort.go
+++ algorithms/sort/QuickSort.go
@@ -5,7 +5,7 @@ func QuickSort(list []int) {
 	if len(list) <= 1 {
 		return
 	}
-	p := partition(list)
+	p := pivot(list)
 	left, right := 0, len(list)-1
 	list[left], list[p] = list[p], list[left]
 	for left != right {
@@ -21,6 +21,7 @@ func QuickSort(list []int) {
 	QuickSort(list[left+1:])
 }
 
-func partition(list []int) int {
-	return len(list) - 1 //rand.Int() % len(list)
+func pivot(list []int) int {
+	//return rand.Int() % len(list)
+	return len(list) - 1
 }
